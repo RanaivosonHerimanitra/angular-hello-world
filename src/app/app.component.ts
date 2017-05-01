bf9f71f12ee72705af47d8d5cbb69acd73cf0963
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2017-05-01 20:56:45 -0300

    adding hello-world components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <p>Hello world works fine!</p>
+  `
 })
 export class AppComponent {
   title = 'app works!';
